8594663ffd7ba5288d70963a28e55e877dc5fd20
jotasenator <jotasenator@gmail.com>
2022-05-12 19:22:20 -0400

    Implement timeZone Arrays in 2 ways

--- src/components/SelectTimeZones.vue
+++ src/components/SelectTimeZones.vue
@@ -2,31 +2,56 @@
   
   <select v-model="selected">
     <option disabled value="">Seleccione un elemento</option>
-    <option :key="abbr" v-for="abbr in rawTimeZones()">{{abbr['name'] +' '+'(' + abbr['abbreviation'] + ')' }} </option>
+    <option :key="abbr" v-for="abbr in arrayTimeZonesAbrr()">{{abbr}} </option>
     >
   </select>
   <br/>
   <br/>
   <div>Seleccionado: {{ selected }}</div>
+  <br/>
+  <br/>
+  {{arrayTimeZonesAbrr()}}
+  <br/>
+  <br/>
+  <h1>from array</h1> 
+  {{arrayTZabrr()}}
+  <br/>
+  <br/>
+  {{arrayTimeZones()}}
+  <br/>
+  <br/>
+  <h1>from array</h1> 
+  {{arrayTZ()}}
+  
+  {{console.log(rawTimeZones())}}
+  
+  
 </template>
 
 <script>
 import {  rawTimeZones } from "@vvo/tzdb";
+import { arrayTimeZones, arrayTimeZonesAbrr} from '../helpers/timeZones'
+import { arrayTZ, arrayTZabbr} from '../helpers/arrayTimeZone'
+
 
 export default {
     name:'select-timezone',
     data(){
         return {
             selected: '',
-            parsedTimeZones:[]
         }
     },
-    methods:{
-        
+    methods:{        
         rawTimeZones:()=> rawTimeZones,
-        
-
-    }
+        arrayTimeZones:()=> arrayTimeZones,
+        arrayTimeZonesAbrr:()=>arrayTimeZonesAbrr,
+        arrayTZabrr:()=>arrayTZabbr,
+        arrayTZ:()=>arrayTZ,
+    },
+    computed: {
+      console: () => console,
+      
+    },
 
 }
 </script>
