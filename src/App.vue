b6049c07de6f33012d1becce1b37b0de3048e554
jotasenator <jotasenator@gmail.com>
2022-05-12 18:33:54 -0400

    Hasta aqui pincha el seletor simple

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SelectTimeZones/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectTimeZones from './components/SelectTimeZones.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SelectTimeZones
+}
 }
 </script>
 
---
+++ src/components/SelectTimeZones.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- <div>{{timeZones()}} </div>
+  <br/>-->
+  <div>{{rawTimeZones()}} </div>
+  <br/><!--
+  <div>{{timeZonesNames()}} </div>
+  <br/>--><!--
+  <div>{{abbreviations()}} </div> 
+  <br/>-->
+  <select v-model="selected">
+    <option disabled value="">Seleccione un elemento</option>
+    <option :key="abbr" v-for="abbr in rawTimeZones()">{{abbr['name'] +' '+'(' + abbr['abbreviation'] + ')' }} </option>
+    >
+  </select>
+  <div>Seleccionado: {{ selected }}</div>
+</template>
+
+<script>
+import { getTimeZones, rawTimeZones, timeZonesNames, abbreviations } from "@vvo/tzdb";
+
+export default {
+    name:'select-timezone',
+    data(){
+        return {
+            selected: '',
+            parsedTimeZones:[]
+        }
+    },
+    methods:{
+        timeZones:()=> getTimeZones()[0],
+        rawTimeZones:()=> rawTimeZones,
+        timeZonesNames:()=> timeZonesNames,
+        abbreviations:()=> abbreviations,
+        
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
